bd353483facbd631205168152442f9e3cca0ebb3
raj004 <rajshekhartiwari14@gmail.com>
2020-02-18 17:42:07 +0530

    Third Build Add profile imagein half screeen

--- app/src/main/java/com/example/kotlincard_footer/HeaderAdapter.kt
+++ app/src/main/java/com/example/kotlincard_footer/HeaderAdapter.kt
@@ -17,15 +17,18 @@ class HeaderAdapter(internal var data: ArrayList<Data>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         if (viewType == TYPE_ITEM) {
             //inflate your layout and pass it to view holder
-            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_new, parent, false)
+            val itemView =
+                LayoutInflater.from(parent.context).inflate(R.layout.card_new, parent, false)
             return VHItem(itemView)
         } else if (viewType == TYPE_HEADER) {
             //inflate your layout and pass it to view holder
-            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.header_item, parent, false)
+            val itemView =
+                LayoutInflater.from(parent.context).inflate(R.layout.header_item, parent, false)
             return VHHeader(itemView)
         }
         throw RuntimeException("there is no type that matches the type $viewType + make sure your using types correctly")
     }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is VHItem) {
             val dataItem = getItem(position)
@@ -35,10 +38,9 @@ class HeaderAdapter(internal var data: ArrayList<Data>) :
             holder.icon_next.setImageResource(dataItem.imageId2)
 
 
-            if(position.equals(1)){
-                holder.itemView.setPadding(0, 50, 0, 100);
-            }
-            else if(position.equals(6)){
+            if (position.equals(1)) {
+                holder.itemView.setPadding(0, 50, 0, 60);
+            } else if (position.equals(6)) {
 
                 holder.icon_next.setVisibility(View.INVISIBLE);
                 holder.title.setTextColor(Color.parseColor("#F10F2A"))
@@ -47,18 +49,20 @@ class HeaderAdapter(internal var data: ArrayList<Data>) :
             }
 
         } else if (holder is VHHeader) {
-            val row =data[position]
-            holder.titleheader.text=row.title_header
+            val row = data[position]
+            holder.titleheader.text = row.title_header
         }
     }
 
     override fun getItemCount(): Int {
         return data.size + 1
     }
+
     override fun getItemViewType(position: Int): Int {
         return if (isPositionHeader(position)) TYPE_HEADER else TYPE_ITEM
 
     }
+
     private fun isPositionHeader(position: Int): Boolean {
         return position == 0
     }
@@ -66,9 +70,11 @@ class HeaderAdapter(internal var data: ArrayList<Data>) :
     private fun getItem(position: Int): Data {
         return data[position - 1]
     }
+
     fun setClickListener(clickListener: ClickListener) {
         this.clickListener = clickListener
     }
+
     internal inner class VHItem(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
         var title: TextView
         var icon: ImageView
@@ -95,19 +101,23 @@ class HeaderAdapter(internal var data: ArrayList<Data>) :
             }
         }
     }
+
     internal inner class VHHeader(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
         var titleheader: TextView
         private val main_header: LinearLayout
 
         init {
             titleheader = itemView!!.findViewById<View>(R.id.txtHeader) as TextView
-            main_header=itemView!!.findViewById<View>(R.id.main_header) as LinearLayout
+            main_header = itemView!!.findViewById<View>(R.id.main_header) as LinearLayout
             main_header.setOnClickListener {
-                Toast.makeText(itemView.context,
-                    "Postion Header"+ Integer.toString(position),Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    itemView.context,
+                    "Postion Header" + Integer.toString(position), Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
+
     companion object {
         private val TYPE_HEADER = 0
         private val TYPE_ITEM = 1
